14267e51659c165b1b9c2d36a7bcac892d921eea
samcoder140 <sammiller85@protonmail.com>
2022-06-18 07:16:25 -0700

    fix import

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
 import { stripePromise } from './utils/stripe/stripe.utils';
 
 const rootElement = document.getElementById('root');
